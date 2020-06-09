fab18cf1040ec0ce9cd2d41e329d818a8d8c6f7f
Matt Lacey <matt@mrlacey.co.uk>
2020-06-09 21:07:31 +0100

    version to 1.0

--- DontCopyAlways/Properties/AssemblyInfo.cs
+++ DontCopyAlways/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.9.*")]
-[assembly: AssemblyFileVersion("0.9.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
