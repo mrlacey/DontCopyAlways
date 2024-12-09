f7dbebc01c74400736d2b8717ab72be9f20f8b38
Matt Lacey <matt@mrlacey.co.uk>
2024-12-09 09:15:53 +0000

    update analytics data

--- DontCopyAlways/DontCopyAlwaysPackage.cs
+++ DontCopyAlways/DontCopyAlwaysPackage.cs
@@ -72,6 +72,8 @@ namespace DontCopyAlways
 				{
 					{ "VsixVersion", Vsix.Version },
 					{ "VsVersion", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.GetSharedProperty("VS.Core.ExeVersion") },
+					{ "Architecture", RuntimeInformation.ProcessArchitecture.ToString() },
+					{ "MsInternal", Microsoft.VisualStudio.Telemetry.TelemetryService.DefaultSession?.IsUserMicrosoftInternal.ToString() },
 				};
 
 			client.TrackEvent(Vsix.Name, properties);
